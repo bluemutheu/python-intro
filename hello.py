8b179e6c4a4ea89e4e633597b5fb579e226e24c2
bluemutheu <mutheudiana12@gmail.com>
2020-08-06 18:29:33 +0300

    First commit

---
+++ hello.py
@@ -0,0 +1,53 @@
+print("Hello World")
+x = 1+2
+print(x)
+bootcamp = 'PythonProgramming'
+print(bootcamp)
+print(bootcamp[8])
+print(bootcamp[5:8])
+# question1
+learn = 'We are learning how to program in python. python is fun.'
+print(learn)
+meet= learn.count('python')
+print(meet)
+# quetsion2
+okay= 'reven'
+print(okay)
+live= okay[::-1]
+print(live)
+# question3
+name = 'umbrella'
+life= 'e' in name
+print(life)
+# question4
+a = "I"
+b = " cant"
+c = " wait"
+d = " to be"
+e = " a"
+f = " professional"
+g = " python"
+h = " programmer"
+z= a+b+c+d+e+f+g+h
+print(z)
+# question5
+nice= 'The worlds fastest plane'
+print(nice)
+print(nice[0])
+# question6
+alvin= 'my name is dee'
+print(alvin)
+diana= alvin.isupper()
+print(diana)
+# question7
+lex= alvin.islower()
+print(lex)
+# question8
+fname= input("Enter firstname:")
+sname = input("Enter secondname:")
+surname = input("Enter lastname:")
+node= (fname[0])
+axe= (sname[0])
+load= (surname)
+initials=(node.capitalize()+'.'+axe.capitalize()+'.'+load.capitalize()+'.')
+print(initials)
